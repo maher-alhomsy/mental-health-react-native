492272155a2bcc589b1ecb5e4276450e162c1eb0
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-05-11 00:54:34 +0300

    add signOut logic

--- app/(app)/(authenticated)/(tabs)/profile.tsx
+++ app/(app)/(authenticated)/(tabs)/profile.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, Text, Pressable } from 'react-native';
+
+import { useAuth } from '@/providers/AuthProvider';
 
 const Page = () => {
+  const { signOut } = useAuth();
+
+  const handleSignOut = async () => {
+    await signOut();
+  };
+
   return (
-    <View>
+    <View className="flex-1 justify-center items-center">
       <Text>Page</Text>
+
+      <Pressable className="bg-red-500 p-4 rounded-lg" onPress={handleSignOut}>
+        <Text className="text-white text-center font-semibold">Sign Out</Text>
+      </Pressable>
     </View>
   );
 };
