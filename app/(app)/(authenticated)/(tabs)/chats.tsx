36cec62398daa5110ddc045acdd6d3a34b7260a0
maher-alhomsy <maheralhomsy2002@gmail.com>
2025-05-10 20:54:05 +0300

    show channel list in chats screen

--- app/(app)/(authenticated)/(tabs)/chats.tsx
+++ app/(app)/(authenticated)/(tabs)/chats.tsx
@@ -1,10 +1,73 @@
-import { View, Text } from 'react-native';
-import React from 'react';
+import { View, TouchableOpacity } from 'react-native';
+
+import {
+  ChannelList,
+  useChatContext,
+  ChannelPreviewMessenger,
+} from 'stream-chat-expo';
+import { Ionicons } from '@expo/vector-icons';
+import { Link, router, Stack } from 'expo-router';
+
+import { useAuth } from '@/providers/AuthProvider';
 
 const Page = () => {
+  const { isTherapist } = useAuth();
+  const { client } = useChatContext();
+
+  // all chats where I part of it
+  const filter = {
+    members: {
+      $in: [client.user!.id],
+    },
+  };
+
+  const options = {
+    presence: true,
+    state: true,
+    watch: true,
+  };
+
+  const CustomListItem = (props: any) => {
+    const { unread } = props;
+    const backgroundColor = unread ? 'bg-blue-100' : 'bg-white';
+
+    return (
+      <View className={`${backgroundColor}`}>
+        <ChannelPreviewMessenger {...props} />
+      </View>
+    );
+  };
+
   return (
-    <View>
-      <Text>Page</Text>
+    <View className="flex-1">
+      <Stack.Screen
+        options={{
+          headerRight: () => (
+            <>
+              {!isTherapist && (
+                <Link href="/(app)/(authenticated)/(modal)/create-chat" asChild>
+                  <TouchableOpacity className="mr-4">
+                    <Ionicons
+                      size={24}
+                      color="cyan"
+                      name="add-circle-outline"
+                    />
+                  </TouchableOpacity>
+                </Link>
+              )}
+            </>
+          ),
+        }}
+      />
+
+      <ChannelList
+        filters={filter}
+        options={options}
+        onSelect={(channel) =>
+          router.push(`/(app)/(authenticated)/chat/${channel.id}`)
+        }
+        Preview={CustomListItem}
+      />
     </View>
   );
 };
